7914815b6ed6718d3e30f6369364c06d56c3dcf8
Martin Dederer <md@spielmeister.com>
2013-08-22 18:20:47 +0200

    Fix documentation of Math object

--- builtin_types.js
+++ builtin_types.js
@@ -79,89 +79,89 @@ Math = {};
 /**
  * Returns the absolute value (modulus) of a number.
  */
-Math.abs = function() {}
+Math.abs = function() {};
 
 /**
  * Returns the arccosine of a number.
  */
-Math.acos = function() {}
+Math.acos = function() {};
 
 /**
  * Returns the arcsine of a number.
  */
-Math.asin = function() {}
+Math.asin = function() {};
 
 /**
  * Returns the arctangent of a number.
  */
-Math.atan = function() {}
+Math.atan = function() {};
 
 /**
  * Returns the arctangent of the quotient of its arguments.
  */
-Math.atan2 = function() {}
+Math.atan2 = function() {};
 
 /**
  * Returns the smallest integer greater than or equal to a number.
  */
-Math.ceil = function() {}
+Math.ceil = function() {};
 
 /**
  * Returns the cosine of a number.
  */
-Math.cos = function() {}
+Math.cos = function() {};
 
 /**
  * Returns Enumber, where number is the argument, and E is Euler's constant (2.718...), the base of the natural logarithm.
  */
-Math.exp = function() {}
+Math.exp = function() {};
 
 /**
  * Returns the largest integer less than or equal to a number.
  */
-Math.floor = function() {}
+Math.floor = function() {};
 
 /**
  * Returns the natural logarithm (loge, also ln) of a number.
  */
-Math.log = function() {}
+Math.log = function() {};
 
 /**
  * Returns the largest of zero or more numbers.
  */
-Math.max = function() {}
+Math.max = function() {};
 
 /**
  * Returns the smallest of zero or more numbers.
  */
-Math.min = function() {}
+Math.min = function() {};
 
 /**
  * Returns base to the exponent power, that is, base^exponent.
  */
-Math.pow = function() {}
+Math.pow = function() {};
 
 /**
  * Returns a pseudo-random number between 0 and 1.
  */
-Math.random = function() {}
+Math.random = function() {};
 
 /**
  * Returns the value of a number rounded to the nearest integer.
  */
-Math.round = function() {}
+Math.round = function() {};
 
 /**
  * Returns the sine of a number.
  */
-Math.sin = function() {}
+Math.sin = function() {};
 
 /**
  * Returns the positive square root of a number.
  */
-Math.sqrt = function() {}
+Math.sqrt = function() {};
 
 /**
  * Returns the tangent of a number.
  */
-Math.tan = function() {}
+Math.tan = function() {};
